baeff3f0de191525b58c2453e6aa1e4199009bf3
James Simpson III <jamescsimpson98@gmail.com>
2022-12-12 17:50:27 -0600

    Initial commit of cdk app for the Cloud Resume Challenge

--- cloud_resume_challenge_app/cloud_resume_challenge_app_stack.py
+++ cloud_resume_challenge_app/cloud_resume_challenge_app_stack.py
@@ -1,8 +1,13 @@
 from aws_cdk import (
-    # Duration,
+    aws_s3 as s3_,
     Stack,
-    # aws_sqs as sqs,
+    aws_cloudfront as cf_,
+    aws_lambda as lambda_,
+    aws_cloudfront_origins as origins,
+    
+    
 )
+
 from constructs import Construct
 
 class CloudResumeChallengeAppStack(Stack):
@@ -17,3 +22,16 @@ class CloudResumeChallengeAppStack(Stack):
         #     self, "CloudResumeChallengeAppQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        s3 = s3_.Bucket(self, "CloudResumeChallengeBucket",
+            block_public_access=s3_.BlockPublicAccess.BLOCK_ALL,
+        )
+        
+        distribution = cf_.CloudFrontWebDistribution(self, 
+            default_behavior=cf_.BehaviorOptions(origin=origins.S3Origin(s3))
+            default_root_object="index.html",
+            allowed_methods=cf_.AllowedMethods.ALLOW_ALL,
+
+            viewer_protocol_policy=cf_.cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+        )
+
+        
